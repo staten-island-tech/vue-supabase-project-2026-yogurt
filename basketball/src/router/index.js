44ffdc78172cf555eb4398ce486e1956aaf0ac01
uni897 <akhasmeer7@gmail.com>
2026-04-29 13:43:27 -0400

    gurt

--- basketball/src/router/index.js
+++ basketball/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import base from '@/views/base.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'base',
+      component: base
+    }
+  ],
 })
 
 export default router
---
+++ basketball/src/views/base.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        gurt: yo
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
