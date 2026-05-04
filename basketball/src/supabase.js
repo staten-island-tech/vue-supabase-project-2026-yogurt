b7b7d677593996af587d1b865f82673e3d48162b
owenc216 <owen216413@gmail.com>
2026-05-04 13:57:24 -0400

    cfh

--- basketball/src/supabase.js
+++ basketball/src/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const VITE_SUPABASE_URL = "https://yruvfbjilqwvlyngqtua.supabase.co"
-const VITE_SUPABASE_PUBLISHABLE_KEY ="sb_publishable_jU8Qkw81XrAjIxgJfwkdNw_VEsm6O9d"
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
