4653c7aa191580fd46fb028b179dee62f76f8739
uni897 <akhasmeer7@gmail.com>
2026-05-07 14:01:10 -0400

    beta

--- basketball/src/views/login.vue
+++ basketball/src/views/login.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="bg-amber-500 w-50 h-100 flex flex-wrap justify-center items-center">
         Login
-        <input v-model="user" placeholder="Please type your username" class="w-50"></input>
+        <input v-model="name" placeholder="Please type your name" class="w-50"></input>
+        <input v-model="user" placeholder="Please type your email" class="w-50"></input>
         Password
         <input v-model="key" placeholder="Please type your password" class="w-50"></input>
         <button @click="login" class="w-30 h-10 bg-red-400 hover:cursor-pointer">Login</button>
@@ -14,8 +15,15 @@ import { ref } from 'vue'
 
 const user = ref("")
 const key = ref("")
-function login(){
+const name = ref("")
+async function login(){
     console.log(user.value, key.value)
+    
+    let { data, error } = await supabase
+    .from('profiles')
+    .select('id,name')
+    console.log(data, error)
+  
 }
 
 console.log(supabase)
