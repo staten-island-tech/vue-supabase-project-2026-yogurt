e7c2c6760447b7b22b4f3360c909b56e31b54b2a
owenc216 <owen216413@gmail.com>
2026-05-15 13:53:31 -0400

    kndzkugavd q

--- basketball/src/views/login.vue
+++ basketball/src/views/login.vue
@@ -6,6 +6,7 @@
         Password
         <input v-model="key" placeholder="Please type your password" class="w-50"></input>
         <button @click="login" class="w-30 h-10 bg-red-400 hover:cursor-pointer">Login</button>
+        <button @click="signUp" class="w-30 h-10 bg-red-400 hover:cursor-pointer">Sign Up</button>
     </div>
 </template>
 
@@ -16,7 +17,7 @@ import { ref } from 'vue'
 const user = ref("") 
 const key = ref("")
 const name = ref("")
-async function login(){
+/* async function login(){
     console.log(name.value, user.value, key.value)
     
     let { data, error } = await supabase
@@ -34,8 +35,55 @@ async function login(){
     }else{
         console.log("Enter email/name")
     }   
-}
+} */
+async function upsertProfile() {
+  const { data: userData, error: userError } = await supabase.auth.getUser()
+  if (userError) throw userError
 
+  const { error } = await supabase
+    .from('profiles')
+    .upsert(
+      {
+        id: userData.user.id,          // MUST match RLS: id = auth.uid()
+        email: userData.user.email,
+        name: name.value || null,
+      },
+      { onConflict: 'id' }           // id is your PK
+    )
+
+  if (error) throw error
+}
+async function login() {
+  const { error } = await supabase.auth.signInWithPassword({
+    email: user.value,
+    password: key.value,
+  })
+
+  if (error) {
+    alert(error.message)
+    return
+  }
+
+  await upsertProfile()
+}
+async function signUp() {
+  const { error } = await supabase.auth.signUp({
+    email: user.value,
+    password: key.value,
+    options: {
+      data: {
+        name: name.value || null,
+      },
+    },
+  })
+
+  if (error) {
+    alert(error.message)
+    return
+  }
+
+  alert("Check your email to confirm, if confirmation is enabled.")
+}
 //console.log(supabase)
 </script>
 
