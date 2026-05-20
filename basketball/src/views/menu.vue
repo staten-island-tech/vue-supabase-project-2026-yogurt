8dce517ee5009856d51b2abd0752a08e9dcf5835
uni897 <akhasmeer7@gmail.com>
2026-05-20 14:02:16 -0400

    asdas

--- basketball/src/views/menu.vue
+++ basketball/src/views/menu.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-
+    <div class="bg-teal-200 h-screen">
+        <div class="w-full max-w-md justify-center items-center p-25 flex-col flex bg-red-500">
+            <RouterLink class="bg-red-300 rounded-2xl border-2 w-50 p-5">Play</RouterLink>
+            <RouterLink class="p-5">Choose Team</RouterLink>
+            <RouterLink class="p-5">Idk something be here</RouterLink>
+        </div>
     </div>
 </template>
 
@@ -8,6 +12,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
