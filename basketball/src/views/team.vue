fb30dd51839ab819aa7534e73f454dfab1a1d593
owenc216 <owen216413@gmail.com>
2026-05-28 13:50:21 -0400

    ad

--- basketball/src/views/team.vue
+++ basketball/src/views/team.vue
@@ -18,6 +18,12 @@
     team 3
     </div>
     <RouterLink to="/menu" class="p-5">Back To Menu</RouterLink>
+    select a team
+    <select v-model="selectedSchool">
+      <option value="">team 1</option>
+      <option value="">team 2</option>
+      <option value="">team 3</option>
+    </select>
 </template>
 
 <script setup>
