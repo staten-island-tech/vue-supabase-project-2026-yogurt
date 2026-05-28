8d4719190654e26c19f5fba3b6c6fac578c417fa
uni897 <akhasmeer7@gmail.com>
2026-05-28 13:35:06 -0400

    gfurt

--- basketball/src/views/roll.vue
+++ basketball/src/views/roll.vue
@@ -12,12 +12,13 @@
             <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254"></img>
             Lebron
         </div>
+        <card class="w-50 h-100 bg-gray-200 rounded-2xl mr-10" :name="lebron" :img="'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254'"></card>
     </div>
     <button>Roll</button>
 </template>
 
 <script setup>
-
+import card from '@/components/card.vue'
 </script>
 
 <style scoped>
