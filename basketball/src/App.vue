cc964a2a66c0dd8ffd912f435749b51053e40599
owenc216 <owen216413@gmail.com>
2026-05-28 13:47:18 -0400

    eqwd

--- basketball/src/App.vue
+++ basketball/src/App.vue
@@ -1,10 +1,8 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <RouterView></RouterView>
-  
 </template>
 
 <style scoped></style>
+<!-- 2k_7azburbns6ka0mo63krjl8stmt6e3qod -->
--- basketball/src/views/team.vue
+++ basketball/src/views/team.vue
@@ -1,16 +1,27 @@
 <template>
-    <div>
-        Team 1
-        <div>
-            
+    team 1
+  <div class="flex flex-row">
+        <div class="w-50 h-100 bg-gray-200 rounded-2xl mr-10">
+            <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254"></img>
+            Lebron
         </div>
+        <div class="w-50 h-100 bg-gray-200 rounded-2xl mr-10">
+            <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254"></img>
+            Lebron
+        </div>
+        <div class="w-50 h-100 bg-gray-200 rounded-2xl mr-10">
+            <img src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254"></img>
+            Lebron
+        </div>
+        <card class="w-50 h-100 bg-gray-200 rounded-2xl mr-10" :name="lebron" :img="'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png&w=350&h=254'"></card>
+    team 2
+    team 3
     </div>
+    <RouterLink to="/menu" class="p-5">Back To Menu</RouterLink>
 </template>
 
 <script setup>
-
+import card from '@/components/card.vue'
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
